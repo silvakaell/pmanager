dfd0abc038aec6d91a789f214bebbea3f09c9aa6
Kaell Fernandes <silvakaell@gmail.com>
2023-11-20 09:42:49 -0300

    v.03

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(width=200, height=200)
+image = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=image)
+
+canvas.pack()
+
+window.mainloop()
